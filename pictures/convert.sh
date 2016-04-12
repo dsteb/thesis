cf3e59f1b696491afdb0d6ed6459a1256bf8a95b
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-04-12 10:44:36 +0200

    Prefinal version

--- pictures/convert.sh
+++ pictures/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # example run: ./convert.sh mape
 convert $1.png eps2:$1.eps
-convert $1.png $1.pnm
-potrace $1.pnm -s -o $1.svg
-convert $1.png $1.pdf
-rm $1.pnm
+# convert $1.png $1.pnm
+# potrace $1.pnm -s -o $1.svg
+# convert $1.png $1.pdf
+# rm $1.pnm
